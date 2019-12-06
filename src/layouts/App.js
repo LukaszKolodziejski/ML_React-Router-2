287008471dc559b13762e429f2cf1b184812e4ec
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2019-12-06 16:54:38 +0100

    change basename

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './Header';
-import Navigation from './Navigation';
-import Page from './Page';
-import Footer from './Footer';
+import React, { Component } from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="app">
-          <header>
-            {<Header />}
-          </header>
+          <header>{<Header />}</header>
           <main>
-            <aside>
-              {<Navigation />}
-            </aside>
-            <section className="page">
-              {<Page />}
-            </section>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
           </main>
           <footer>{<Footer />}</footer>
         </div>
