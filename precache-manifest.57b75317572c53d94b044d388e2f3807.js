8265f08072000f16f5717a4d715b36927f678b3e
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2019-12-06 08:13:23 +0100

    Updates

---
+++ precache-manifest.57b75317572c53d94b044d388e2f3807.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1510a0491341207c628b",
+    "url": "/React-Router-2/static/css/main.49b6d18e.chunk.css"
+  },
+  {
+    "revision": "1510a0491341207c628b",
+    "url": "/React-Router-2/static/js/main.1510a049.chunk.js"
+  },
+  {
+    "revision": "30bd958ba7e0e8189f2a",
+    "url": "/React-Router-2/static/js/runtime~main.30bd958b.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/React-Router-2/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/React-Router-2/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/React-Router-2/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/React-Router-2/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "379c5e64e5a2696b9b205e386eeb301b",
+    "url": "/React-Router-2/index.html"
+  }
+];
